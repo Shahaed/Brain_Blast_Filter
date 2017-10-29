6d54809f69705d9e50d82934813e11841fd1d1e5
Shahaed Hasan <shahaed-hasan@uiowa.edu>
2017-10-29 00:04:48 -0500

    1st

---
+++ brainQuery.py
@@ -0,0 +1,28 @@
+import pymysql.cursors
+
+def queryAccel():
+
+    # connecting to thomas han database
+    connection = pymysql.connect(host='phly.c7jx0v6pormd.us-east-1.rds.amazonaws.com',
+                                 user='phly',
+                                 password='phlyisthebest',
+                                 db='phly',
+                                 port=3306,
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    try:
+
+        with connection.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT * FROM accel_data"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+    finally:
+        connection.close()
+
+    print(len(result))
+    print(result)
+    return result
+
+if __name__ == '__main__':
+    queryAccel()
---
+++ mathUtl.py
@@ -0,0 +1,7 @@
+import math
+
+def magnitude( x, y, z):
+    return math.sqrt(x**2+y**2+z**2)
+
+
+
